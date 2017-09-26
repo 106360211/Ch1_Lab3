5793a688449f9a4ec2817db38e01a4eeb8f018f1
106360211 <justin0010523@gmail.com>
2017-09-26 16:26:55 +0800

    23

---
+++ source/main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int n1;
+	int n2;
+
+	printf("enter two integers,and tell you\n");
+	printf("the relationships they satisfy:");
+
+	scanf_s("%d%d",&n1,&n2);
+
+	if (n1 == n2)
+	{
+		printf("%d is equal to %d\n",n1,n2);
+
+	}
+	if (n1 != n2)
+	{
+		printf("%d is not equal to %d\n", n1, n2);
+
+	}
+	
+	if (n1 < n2)
+	{
+		printf("%d is less than %d\n", n1, n2);
+
+	}
+	
+	if (n1 > n2)
+	{
+		printf("%d is greater than %d\n", n1, n2);
+
+	}
+	
+	if (n1 <= n2)
+	{
+		printf("%d is less than or equal to %d\n", n1, n2);
+
+	}
+
+	if (n1 >= n2)
+	{
+		printf("%d is greater than or equal to %d\n", n1, n2);
+
+	}
+	system("pause");
+	return 0;
+
+}
